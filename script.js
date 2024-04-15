b7da99f19b635f13e61aa0237103aa862e4cebd5
jvillem <vallemjuri@gmail.com>
2024-04-15 19:09:00 +0300

    added camera that follows player

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-let gameHeight = window.innerHeight;
-let gameWidth = window.innerWidth;
-let gameWindow = document.getElementById('gameWindow');
-gameWindow.style.height = `${gameHeight}px`;
-gameWindow.style.width = `${gameWidth}px`;
-gameWindow.style.backgroundColor = 'black';
+const gameHeight = 500;
+const gameWidth = 5000;
 
 class Obstacle {
     constructor() {
@@ -11,6 +7,7 @@ class Obstacle {
         this.y = Math.floor(Math.random() * gameHeight-50);
         this.width = 50;
         this.height = 50;
+
         this.div = document.createElement('div');
         this.div.style.width = `${this.width}px`;
         this.div.style.height = `${this.height}px`;
@@ -18,11 +15,12 @@ class Obstacle {
         this.div.style.position = 'absolute';
         this.div.style.left = `${this.x}px`;
         this.div.style.top = `${this.y}px`;
-        gameWindow.appendChild(this.div);
+        level.div.appendChild(this.div);
     }
 
+    // removes the obstacle from the level
     death() {
-        gameWindow.removeChild(this.div);
+        level.div.removeChild(this.div);
     }
 }
 
@@ -44,6 +42,14 @@ class Player {
     }
 
     characterLoop() {
+        this.movement();
+        this.physics();
+        this.colisions();
+        this.updatePlayerPos();
+    }
+
+    // handles the movement of the character
+    movement() {
         if (this.up && !this.isJumping) {
             this.y_velocity -= 40;
             this.isJumping = true;
@@ -57,14 +63,18 @@ class Player {
             this.x_velocity += this.speed;
         }
 
-        this.y_velocity += 1.75; // gravity
         this.x += this.x_velocity;
         this.y += this.y_velocity;
-        
+    }
+
+    physics() {
+        this.y_velocity += 1.75; // gravity
         this.x_velocity *= 0.9; // friction
         this.y_velocity *= 0.9; // friction
-        
-        // colision detection
+    }
+
+    colisions() {
+        // colision detection with obstacles
         obstacleArray.forEach(obs => {
             if (colision(this.div.getBoundingClientRect(), obs.div.getBoundingClientRect())) {
                 this.colisionSide(this.div.getBoundingClientRect(), obs.div.getBoundingClientRect(), obs);
@@ -89,9 +99,22 @@ class Player {
             this.x = gameWidth - this.width;
             this.x_velocity = 0;
         }
-        
+    }
+    // moves the character in the level
+    updatePlayerPos() { 
         this.div.style.left = `${this.x}px`;
         this.div.style.top = `${this.y}px`;
+
+        // moves the camera
+        this.updateCameraPos(this);
+    }
+
+    updateCameraPos(player) {
+        if (player.x < 500) {
+            level.div.style.left = '0px';
+        } else {
+            level.div.style.left = `${-player.x + 500}px`;
+        }
     }
     
     // checks the what side of the object the coliision is happening
@@ -104,26 +127,27 @@ class Player {
                 // bottom
                 obs.death();
                 this.y_velocity = 0;
-                this.y = obj2Rect.bottom;
+                this.y = obs.y + obs.height;
             } else { 
                 // top
                 this.isJumping = false;
                 this.y_velocity = 0;
-                this.y = obj2Rect.top - this.height;
+                this.y = obs.y - obs.height;
             }
         } else {
             if (vectorX > 0) {
                 // left
                 this.x_velocity = 0;
-                this.x = obj2Rect.right;
+                this.x = obs.x + obs.width;
             } else {
                 // right
                 this.x_velocity = 0;
-                this.x = obj2Rect.left - this.width;
+                this.x = obs.x - obs.width;
             }
         }
     }
 
+    // checks the key pressed
     keyListener(event) {
         let key_state = (event.type === 'keydown') ? true : false;
             switch(event.key) {
@@ -140,20 +164,29 @@ class Player {
     }
 }
 
+
 class Level {
     constructor() {
-        
+        // game window
+        this.div = document.getElementById('gameWindow');
+        this.div.style.height = `${gameHeight}px`;
+        this.div.style.width = `${gameWidth}px`;
+
+        // camera
+        this.camera = document.getElementsByClassName('camera')[0];
+        this.camera.style.height = `500px`;
+        this.camera.style.width = `1000px`;
     }
 }
-
-let player = new Player();
-
-let obstacleArray = [new Obstacle()];
 
 const colision = (obj1, obj2) => obj1.left < obj2.right &&
     obj1.right > obj2.left &&
     obj1.top < obj2.bottom &&
-    obj1.bottom > obj2.top
+    obj1.bottom > obj2.top;
+
+let level = new Level();
+let player = new Player();
+let obstacleArray = [new Obstacle()];
 
 
 // event listeners
