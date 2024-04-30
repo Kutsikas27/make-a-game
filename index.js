0ccc5eb48a233665b704ea29489b4cf1ad91d4d9
Kutsikas27 <mardus80@gmail.com>
2024-04-30 17:02:16 +0300

    fix sounds

--- index.js
+++ index.js
@@ -65,8 +65,9 @@ document.addEventListener("keyup", (e) => {
   }
 });
 let index = 0;
-const deathSound = new Audio("Ghost.mp3");
+const deathSound = new Audio("Death.mp3");
 const bonusSound = new Audio("score.mp3");
+const ghostSound = new Audio("Ghost.mp3");
 const songs = ["theme.mp3", "song2.mp3", "song3.mp3", "song4.mp3", "song5.mp3"];
 const audio = new Audio(songs[index]);
 const volumeSlider = document.getElementById("volume-slider");
@@ -90,12 +91,14 @@ muteBtn.addEventListener("click", () => {
     audio.muted = false;
     deathSound.muted = false;
     bonusSound.muted = false;
+    ghostSound.muted = false;
     muteBtn.innerHTML =
       '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-volume-up" viewBox="0 0 16 16"><path d="M9 4a.5.5 0 0 0-.812-.39L5.825 5.5H3.5A.5.5 0 0 0 3 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 9 12zm3.025 4a4.5 4.5 0 0 1-1.318 3.182L10 10.475A3.5 3.5 0 0 0 11.025 8 3.5 3.5 0 0 0 10 5.525l.707-.707A4.5 4.5 0 0 1 12.025 8"/></svg>';
   } else {
     audio.muted = true;
     deathSound.muted = true;
     bonusSound.muted = true;
+    ghostSound.muted = true;
     muteBtn.innerHTML =
       '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-volume-up" viewBox="0 0 16 16"><path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06m7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0"/></svg>';
   }
@@ -120,7 +123,7 @@ const move = () => {
       if (player.powerUp) {
         ghosts.forEach((ghost, i) => {
           if (ghost.pos === player.pos) {
-            deathSound.play();
+            ghostSound.play();
             ghost.style.backgroundColor = "black";
             ghost.style.opacity = "0.8";
             ghost.pos = game.startGhost[i];
@@ -128,7 +131,6 @@ const move = () => {
           }
         });
       } else {
-        const deathSound = new Audio("Death.mp3");
         deathSound.play();
         player.lives--;
         updateScoreAndLives();
